fe338b0c970aed791cde8a0c36f6ab0c2a9cd408
Hieu Nguyen Thi Trung <hieunguyenthitrung@Hieus-MacBook-Air.local>
2014-06-13 14:56:26 +0700

    Initial Commit

---
+++ SearchPhoneNumber/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SearchPhoneNumber
+//
+//  Created by Hieu Nguyen Thi Trung on 6/13/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SearchPhoneNumberTests/SearchPhoneNumberTests.m
@@ -0,0 +1,34 @@
+//
+//  SearchPhoneNumberTests.m
+//  SearchPhoneNumberTests
+//
+//  Created by Hieu Nguyen Thi Trung on 6/13/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SearchPhoneNumberTests : XCTestCase
+
+@end
+
+@implementation SearchPhoneNumberTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
